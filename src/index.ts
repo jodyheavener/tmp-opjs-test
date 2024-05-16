206732b674e8a7ee4ec6485852dc4c46a1c07bea
Jody Heavener <jody.heavener@agilebits.com>
2024-05-16 11:39:18 -0600

    Update example

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-import { whoami, item, user } from "@1password/op-js";
+import { whoami, item, user, vault, Item } from "@1password/op-js";
 
-// Force an auth before we call whoami
+const sample = <T>(items: T[]) =>
+  items[Math.floor(Math.random() * items.length)];
+
+// Force an auth for all operations
 user.me();
 
+console.log(`Hello, ${whoami()?.email}!`);
+
+const vaults = vault.list();
+const singleVault = sample(vaults);
+
+const vaultItems = item.list({
+  vault: singleVault.id,
+});
+
 console.log(
-  `Hello, ${whoami()?.email}! You have ${
-    item.list().length
-  } items in your account.`
+  `You have ${vaultItems.length} items in the "${singleVault.name}" vault.`
 );
+
+if (vaultItems.length > 0) {
+  const singleItem = item.get(sample(vaultItems).id) as Item;
+  console.log(
+    `Here's a random one: in this vault, the item "${singleItem.title}" has ${
+      (singleItem.fields || []).length
+    } fields.`
+  );
+}
